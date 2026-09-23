6ad6e19e7ecc7ab2c6c3959d1a26a3085cbe4435
alanmary1789-svg <alanmary1789@gmail.com>
2026-09-23 15:56:42 +0200

    Phase 2 : Solveur BFS, Dijkstra et A* avec tests (bidirectionnel reporté pour plus tard)

--- src/ricochet/solvers.py
+++ src/ricochet/solvers.py
@@ -1,9 +1,10 @@
 from dataclasses import dataclass 
 from ricochet.simulation import successors
 from ricochet.table import Table 
-from ricochet.game import Game 
+from ricochet.game import Game, COLORS
 
 from collections import deque
+import heapq, itertools
 import time
 
 @dataclass
@@ -77,4 +78,132 @@ def _reconstruct(parent, etat_final, start, explored):
             elapsed = time.perf_counter() - start
         )
 
+""" 
+def solve_dijkstra(game):
+    start = time.perf_counter()
+    depart, parent, explored = game.start_state(), {depart : None}, 0
+    tas, compteur, cout = [], itertools.count(), {depart : 0}
+    heapq.heappush(tas, (0, next(compteur), depart))
 
+    if game.is_goal(depart):
+        return _reconstruct(parent, depart, start, explored)
+    
+
+    while tas : 
+        u, state_u = heapq.heappop(tas)
+        explored += 1 
+        for coup, v in successors(game.table, u):
+             if v not in parent:
+                cout[v] = cout[u] + 1 
+                parent[v] = (u, coup)
+
+                if game.is_goal(v):
+                     return _reconstruct_dijkstra(parent, v, start, explored)
+
+                heapq.heappush(tas, (cout, etat))
+
+    return None 
+"""
+
+def solve_dijkstra(game):
+    start = time.perf_counter()
+
+    depart = game.start_state()
+    parent = {depart: None}
+    cout = {depart: 0}
+    compteur = itertools.count()
+    tas = [(0, next(compteur), depart)]
+    explored = 0
+
+    while tas:
+        cout_u, _, u = heapq.heappop(tas)
+        if cout_u > cout[u]:          # entrée périmée : un meilleur chemin a déjà été traité
+            continue
+        explored += 1
+
+        if game.is_goal(u):
+            return _reconstruct(parent, u, start, explored)
+
+        for coup, v in successors(game.table, u):
+            nouveau_cout = cout[u] + 1
+            if v not in cout or nouveau_cout < cout[v]:   # relâchement : chemin nouveau ou meilleur
+                cout[v] = nouveau_cout
+                parent[v] = (u, coup)
+                heapq.heappush(tas, (nouveau_cout, next(compteur), v))
+
+    return None
+
+
+
+def _heuristique(game, state):
+    """Minorant du nombre de coups restants : 0 si le robot cible est déjà
+    sur la case, 1 s'il est aligné avec elle (même ligne ou même colonne),
+    2 sinon. Toujours <= au vrai nombre de coups, donc admissible."""
+    x, y = state[COLORS.index(game.target_robot)]
+    tx, ty = game.target_cell
+    if (x, y) == (tx, ty):
+        return 0
+    if x == tx or y == ty:
+        return 1
+    return 2
+
+
+def solve_astar(game):
+    start = time.perf_counter()
+
+    depart = game.start_state()
+    parent = {depart: None}
+    cout = {depart: 0}
+    compteur = itertools.count()
+    tas = [(_heuristique(game, depart), next(compteur), depart)]
+    explored = 0
+
+    while tas:
+        _, _, u = heapq.heappop(tas)
+        if game.is_goal(u):
+            return _reconstruct(parent, u, start, explored)
+        explored += 1
+
+        for coup, v in successors(game.table, u):
+            nouveau_cout = cout[u] + 1
+            if v not in cout or nouveau_cout < cout[v]:
+                cout[v] = nouveau_cout
+                parent[v] = (u, coup)
+                priorite = nouveau_cout + _heuristique(game, v)   # f = g + h
+                heapq.heappush(tas, (priorite, next(compteur), v))
+
+    return None
+
+
+def solve_bbfs(game):
+        start = time.perf_counter() 
+
+        depart, arrive = game.start_state(), (game.target_robot, game.target_cell)
+        parent_1, parent_2 = {depart : None}, {arrive : None} #1 pour depart, 2 pour arrive
+        f1, f2 = deque([depart]), deque([arrive])
+        explored1, explored2 = 0, 0
+
+        if game.is_goal(depart):
+            return _reconstruct_bbfs(parent_1, depart, start, explored1)
+
+        while f1 and f2 : 
+            s1, s2 = f1.popleft(), f2.popleft
+
+            explored1 += 1
+            explored2 += 1
+
+            for coup1, v1 in successors(game.table, s1):
+                if v1 not in parent_1:
+                    parent_1[v1] = (s1, coup1)
+
+                    if game.is_goal(v1):
+                        return _reconstruct(parent_1, v1, start, explored1)
+                    f1.append(v1)
+
+            for coup2, v2 in successors(game.table, s2):
+                if v2 not in parent_1:
+                    parent_2[v2] = (s2, coup2)
+            
+                    
+                    f2.append(v2)
+        return None
--- tests/test_solvers.py
+++ tests/test_solvers.py
@@ -1,7 +1,7 @@
 from ricochet.solvers import Solution 
 from ricochet.table import Table 
-from ricochet.game import Game 
-from ricochet.solvers import solve_bfs 
+from ricochet.game import Game, COLORS
+from ricochet.solvers import solve_bfs, solve_dijkstra, solve_astar 
 
 import pytest 
 
@@ -49,3 +49,21 @@ def test_bfs_unreachable():
              robots={"red": (0, 0), "green": (0, 4), "blue": (4, 4), "yellow": (2, 4)},
              target_robot="red", target_cell=(1, 1))
     assert solve_bfs(g) is None
+
+def test_solvers_agree_on_length():
+    g = make_game(target_cell=(4, 0))
+    n_bfs = len(solve_bfs(g))
+    assert len(solve_dijkstra(g)) == n_bfs
+    assert len(solve_astar(g)) == n_bfs
+
+
+def test_dijkstra_and_astar_unreachable():
+    table = Table(5, 5, name="t")
+    for side in ("N", "S", "E", "W"):
+        table.add_wall(1, 1, side)
+    g = Game(table=table,
+             robots={"red": (0, 0), "green": (0, 4), "blue": (4, 4), "yellow": (2, 4)},
+             target_robot="red", target_cell=(1, 1))
+    assert solve_dijkstra(g) is None
+    assert solve_astar(g) is None
+
